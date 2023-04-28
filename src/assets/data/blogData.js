c827aeb849f167bd9f2dd0a42b2aa5e6b1972fb5
Coder-SlayerPro <code.slayer101.gmail.com>
2023-04-28 16:10:52 +0530

    first

---
+++ src/assets/data/blogData.js
@@ -0,0 +1,41 @@
+import img01 from "../all-Image/blog-img/blog-1.jpg";
+import img02 from "../all-Image/blog-img/blog-2.jpg";
+import img03 from "../all-Image/blog-img/blog-3.jpg";
+
+const blogData = [
+  {
+    id: 1,
+    title: "The best way to drive a car",
+    author: "Hemant",
+    data: "27/03/2023",
+    time: "9pm",
+    imgUrl: img01,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, consequuntur doloremque. Praesentium corporis provident fugiat.",
+    quote: " Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 2,
+    title: "The best way to drive a car",
+    author: "Hemant",
+    data: "18/01/1999",
+    time: "9pm",
+    imgUrl: img02,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, consequuntur doloremque. Praesentium corporis provident fugiat.",
+    quote: " Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+  {
+    id: 3,
+    title: "The best way to drive a car",
+    author: "Hemant",
+    data: "01/01/2023",
+    time: "9pm",
+    imgUrl: img03,
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, consequuntur doloremque. Praesentium corporis provident fugiat.",
+    quote: " Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+  },
+];
+
+export default blogData;
